cfa5b5c16179ae1248630ede8a29cd4b71547161
DiogoJP202 <diogojp202@gmail.com>
2024-12-15 18:54:42 -0300

    Projeto inicial HelloWorld

---
+++ HelloWord/HelloWord/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace HelloWorld
+{
+    class MainClass
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+} 
